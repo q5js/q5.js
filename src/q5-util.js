1c605a2f4ecccf079aa7396533e0db6422b5e961
= <=>
2024-07-24 23:25:36 +0530

    FIXES:#45

--- src/q5-util.js
+++ src/q5-util.js
@@ -31,3 +31,42 @@ Q5.modules.util = ($, p) => {
 	$.minute = () => new Date().getMinutes();
 	$.second = () => new Date().getSeconds();
 };
+
+Q5.load = (...args) => {
+	let p = new Promise((resolve, reject) => {
+		let count = 0;
+		let total = args.length;
+		args.forEach((arg) => {
+			if (typeof arg == 'string') {
+				count++;
+				Q5._loadFile(arg, () => {
+					count--;
+					if (count == 0) resolve();
+				}, 'text');
+			} else if (typeof arg == 'object') {
+				count++;
+				Q5._loadFile(arg.path, () => {
+					count--;
+					if (count == 0) resolve();
+				}, arg.type);
+			}
+		});
+	});
+	return p;
+};	
+
+Q5._loadFile = (path, cb, type) => {
+	p._preloadCount++;
+	let ret = {};
+	fetch(path)
+		.then((r) => {
+			if (type == 'json') return r.json();
+			if (type == 'text') return r.text();
+		})
+		.then((r) => {
+			p._preloadCount--;
+			Object.assign(ret, r);
+			if (cb) cb(r);
+		});
+	return ret;
+};
