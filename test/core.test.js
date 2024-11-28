3e1d7373f36115d510bcca793f9e35b3132b0ef6
Quinton Ashley <qashto@gmail.com>
2024-11-27 19:15:10 -0500

    2.11.2

--- test/core.test.js
+++ test/core.test.js
@@ -1,18 +1,18 @@
 /* node testing */
-// import '../q5-server.js';
+require('../q5-server.js');
 
 /* bun testing */
 // import { expect, test } from 'bun:test';
 // import '../q5-server.js';
 
 /* deno testing */
-import { expect } from '@std/expect';
-import '../q5-deno-server.js';
-let test = Deno.test;
+// import { expect } from '@std/expect';
+// import '../q5-deno-server.js';
+// let test = Deno.test;
 
 let log = console.log;
 
-test(async function coreTest() {
+test('q5-core', async function coreTest() {
 	return new Promise((resolve) => {
 		expect(Q5).toBeDefined();
 
@@ -25,10 +25,14 @@ test(async function coreTest() {
 		expect(q.canvas.width).toBe(200);
 		expect(q.canvas.height).toBe(200);
 
+		let icon = q.loadImage('./q5js_icon.png');
+
 		q.background('silver');
 
 		q.draw = function () {
 			expect(q.frameCount).toBe(1);
+
+			q.image(icon, 36, 36, 128, 128);
 
 			q.canvas.save('test/test.png');
 
